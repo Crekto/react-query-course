b4771df373f36e4649fa4664f364d00fcd7e83f4
Pavel Petronel <41231223+Crekto@users.noreply.github.com>
2023-10-11 16:13:03 +0300

    Finished 15th video - pagination

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -4,15 +4,17 @@ import React from "react";
 import fetchWithError from "../helpers/fetchWithError";
 import Loader from "./Loader";
 
-export default function IssuesList({ labels, status }) {
+export default function IssuesList({ labels, status, pageNum, setPageNum }) {
   const queryClient = useQueryClient();
   const issuesQuery = useQuery(
-    ["issues", { labels, status }],
+    ["issues", { labels, status, pageNum }],
     async ({ signal }) => {
       const statusString = status ? `&status=${status}` : "";
       const labelsString = labels.map((label) => `labels[]=${label}`).join("&");
+      const paginationString = pageNum ? `&page=${pageNum}` : "";
+
       const results = await fetchWithError(
-        `/api/issues?${labelsString}${statusString}`,
+        `/api/issues?${labelsString}${statusString}${paginationString}`,
         {
           signal,
         }
@@ -23,6 +25,9 @@ export default function IssuesList({ labels, status }) {
       });
 
       return results;
+    },
+    {
+      keepPreviousData: true,
     }
   );
 
@@ -81,6 +86,36 @@ export default function IssuesList({ labels, status }) {
               />
             ))}
           </ul>
+          <div className="pagination">
+            <button
+              onClick={() => {
+                if (pageNum - 1 > 0) {
+                  setPageNum(pageNum - 1);
+                }
+              }}
+              disabled={pageNum === 1}
+            >
+              Previous
+            </button>
+            <p>
+              Page {pageNum} {issuesQuery.isFetching ? "..." : ""}
+            </p>
+            <button
+              onClick={() => {
+                if (
+                  issuesQuery.data?.length !== 0 &&
+                  !issuesQuery.isPreviousData
+                ) {
+                  setPageNum(pageNum + 1);
+                }
+              }}
+              disabled={
+                issuesQuery.data?.length === 0 || issuesQuery.isPreviousData
+              }
+            >
+              Next
+            </button>
+          </div>
         </>
       ) : (
         <>
