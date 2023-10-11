5e9c18425d47a4f45a40229d3dcb108cd6fb8aa3
Pavel Petronel <41231223+Crekto@users.noreply.github.com>
2023-10-11 13:25:55 +0300

    Finished 12th video - updating status

--- src/components/IssueDetails.jsx
+++ src/components/IssueDetails.jsx
@@ -3,6 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 import { IssueHeader } from "./IssueHeader";
 import { useUserData } from "../helpers/useUserData";
 import { relativeDate } from "../helpers/relativeDate";
+import IssueStatus from "./IssueStatus";
 
 function useIssueData(issueNumber) {
   return useQuery(["issues", issueNumber], ({ signal }) => {
@@ -68,7 +69,12 @@ export default function IssueDetails() {
                 ))
               )}
             </section>
-            <aside></aside>
+            <aside>
+              <IssueStatus
+                status={issueQuery.data.status}
+                issueNumber={issueQuery.data.number.toString()}
+              />
+            </aside>
           </main>
         </>
       )}
--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -15,7 +15,6 @@ export default function IssuesList({ labels, status }) {
         `/api/issues?${labelsString}${statusString}`,
         {
           signal,
-          headers: { "x-error": true },
         }
       );
 
